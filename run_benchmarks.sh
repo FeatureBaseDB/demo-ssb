dc56c2380ae395eaa6ef1f8130f3de6ed8e2b94e
Alan Bernstein <alan.aaron.bernstein@gmail.com>
2017-09-15 17:01:26 -0500

    Add script to run all benchmarks at once

---
+++ run_benchmarks.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+curl localhost:8000/query/1.1
+curl localhost:8000/query/1.1
+curl localhost:8000/query/1.1
+curl localhost:8000/query/2.1
+curl localhost:8000/query/2.2
+curl localhost:8000/query/2.3
+curl localhost:8000/query/3.1
+curl localhost:8000/query/3.2
+curl localhost:8000/query/3.3
+curl localhost:8000/query/3.4
+curl localhost:8000/query/4.1
+curl localhost:8000/query/4.2
+curl localhost:8000/query/4.3
